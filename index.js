042aeb6c6f9652b7eec6c9e3c3d500225c455008
Alexey Kobevko <folco_2000@mail.ru>
2019-10-15 11:56:10 +0300

    попытка исправить cors. применил body-parser

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -19,8 +20,11 @@ mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${proc
 });
 
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
-app.use(express.json());
 app.use(cors());
 app.options('*', cors());
 
