b4c2986ee3ba403e9f1105bbc5b1717e5fcdef57
Jedi <folco_2000@mail.ru>
2019-10-30 20:21:47 +0300

    add pug

--- app/controllers/verify.controller.js
+++ app/controllers/verify.controller.js
@@ -8,26 +8,35 @@ exports.create = async (req, res) => {
 
     try {
         if (!desiredVerify || !desiredUser) {
-            return res.status(452).send(`Произошла ошибка. Пользователь с id ${req.query.id} не найден`);
+            return res.status(452).render('index', {
+                text: `Произошла ошибка. Пользователь с id ${req.query.id} не найден`,
+            });
         }
         if (req.query.id !== desiredVerify.userId || req.query.id !== desiredUser._id.toString()) {
-            return res.status(452).send('Попытка модификации url-запроса. Пользователь заблокирован.');
+            return res.status(452).render('index', {
+                text: 'Попытка модификации url-запроса. Пользователь заблокирован.',
+            });
         }
         if (desiredVerify.userId !== desiredUser._id.toString()) {
-            return res.status(452).send(`Идентификатор верификации не принадлежит данному пользователю. 
-        Email ${desiredUser.email} заблокирован. Обратитесь в службу поддержки`);
+            return res.status(452).render('index', {
+                text: `Идентификатор верификации не принадлежит данному пользователю. 
+        Email ${desiredUser.email} заблокирован. Обратитесь в службу поддержки`,
+            });
         }
         if (desiredVerify.randomNumber !== req.query.rand) {
-            return res.status(452).send(`Код верификации не принадлежит данному пользователю. 
-        Email ${desiredUser.email} заблокирован. Обратитесь в службу поддержки`);
+            return res.status(452).render('index', {
+                text: `Код верификации не принадлежит данному пользователю. 
+        Email ${desiredUser.email} заблокирован. Обратитесь в службу поддержки`,
+            });
         }
         if (req.query.id === desiredVerify.userId && req.query.rand === desiredVerify.randomNumber) {
             desiredUser.isActive = true;
             await desiredUser.save();
 
             await desiredVerify.remove();
-            return res.status(200).render('index',
-                {text: 'Email успешно подтверждён. Авторизуйтесь в приложении для начала работы'});
+            return res.status(200).render('index', {
+                text: 'Email успешно подтверждён. Авторизуйтесь в приложении для начала работы',
+            });
         }
     }
     catch (e) {
